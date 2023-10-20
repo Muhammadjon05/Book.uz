a1be9fc1cf826a06debe81444b02c9d88942dc1b
Muhammadjon Mahkamboyev <muhammadjonmakhkamov1@gmail.com>
2023-10-20 11:02:32 +0500

    Changes made

--- Book.uz/Entities/Order.cs
+++ Book.uz/Entities/Order.cs
@@ -1,10 +1,12 @@
-﻿namespace Book.uz.Entities;
+﻿using Book.uz.Enums;
+
+namespace Book.uz.Entities;
 
 public class Order
 {
     public Guid OrderId { get; set; }
     public Guid BookId { get; set; }
-    public Book Book { get; set; }
+    public virtual Book Book { get; set; }
     public int Quantity { get; set; }
     public decimal TotalPrice { get; set; }
     public PaymentMethod PaymentMethod { get; set; }
--- Book.uz/Entities/User.cs
+++ Book.uz/Entities/User.cs
@@ -9,4 +9,5 @@ public class User
     public required string Password { get; set; }
     public string PasswordHash { get; set; } = null!;
     public virtual ICollection<UserOrder> UsersOrders { get; set; }
+    public virtual ICollection<Review> Reviews { get; set; }
 }

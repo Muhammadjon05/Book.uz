c0efe456a8e9e48c29cbb76b215aef5c2b8db466
Muhammadjon Mahkamboyev <muhammadjonmakhkamov1@gmail.com>
2023-11-01 13:36:59 +0500

    Changes made

--- Book.uz/Repositories/BookRepository/IBookRepository.cs
+++ Book.uz/Repositories/BookRepository/IBookRepository.cs
@@ -2,6 +2,7 @@
 
 public interface IBookRepository
 {
+    Task<ICollection<uz.Entities.Book>> GetAllTheBooks();
     Task<Entities.Book> AddBook(Entities.Book book);
     Task DeleteBook(Entities.Book book);
     Task<Entities.Book> GetBookById(Guid bookId);

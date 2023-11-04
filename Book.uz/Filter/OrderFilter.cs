43c5f042f641ffdf4a7127b566a563cfb3f58a4d
Muhammadjon Mahkamboyev <muhammadjonmakhkamov1@gmail.com>
2023-11-04 20:23:21 +0500

    Order Fixed

--- Book.uz/Filter/OrderFilter.cs
+++ Book.uz/Filter/OrderFilter.cs
@@ -5,8 +5,8 @@ namespace Book.uz.Filter;
 
 public class OrderFilter : PaginationParams
 {
-    public int Quantity { get; set; }
-    public decimal TotalPrice { get; set; }
-    public PaymentMethod PaymentMethod { get; set; }
-    public PaymentStatus PaymentStatus { get; set; }
+    public int? Quantity { get; set; }
+    public decimal? TotalPrice { get; set; }
+    public PaymentMethod? PaymentMethod { get; set; }
+    public PaymentStatus? PaymentStatus { get; set; }
 }
